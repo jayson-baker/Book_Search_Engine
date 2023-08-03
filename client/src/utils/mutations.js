1efa9b5dcc36ef7ca229e1c0b95bf082eab80d2c
Jayson Baker <bakej07@gmail.com>
2023-08-03 11:42:37 -0500

    Updated the mutations.js with SAVE_BOOK and REMOVE_BOOK. Now able to add and remove books from users

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,12 +13,36 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-    mutation addUser(username: String!, email: String!, password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            _id
-            username
-            email
-            password
-            savedBooks
-        }
-    }`;
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+      password
+      savedBooks
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: String!, $book: String!) {
+    saveBook(userId: $userId, book: $book) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
