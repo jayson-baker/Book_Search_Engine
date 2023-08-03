c812eccdc8350e9cf482abc1ddb22dce6247f18e
Jayson Baker <bakej07@gmail.com>
2023-08-03 10:24:25 -0500

    Added find all Users resolver and find one user based off id. Resolver file updated with login logic.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,34 @@
+const { Book, User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
